01d476e9710c5fa71fe3224029eeecdbd7ecca8b
vspenubarthi <vspenubarthi@wpi.edu>
2018-03-27 14:47:55 -0400

    This is a test commit

---
+++ app/src/main/java/com/mamabearindustries/vegetables/Pantry_Search_Screen.java
@@ -0,0 +1,13 @@
+package com.mamabearindustries.vegetables;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pantry_Search_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantry__search__screen);
+    }
+}
