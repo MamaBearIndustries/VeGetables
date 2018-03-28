735d2e2d54e025dc72784bf75e6b4ec68dff33e2
vspenubarthi <vspenubarthi@wpi.edu>
2018-03-28 12:00:59 -0400

    This has changes to the main activity to make it easier to get to my Page, and it also has a semi-functional search bar

--- app/src/main/java/com/mamabearindustries/vegetables/Pantry_Search_Screen.java
+++ app/src/main/java/com/mamabearindustries/vegetables/Pantry_Search_Screen.java
@@ -2,6 +2,8 @@ package com.mamabearindustries.vegetables;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 public class Pantry_Search_Screen extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class Pantry_Search_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantry__search__screen);
+        final TextView textView = (TextView) findViewById(R.id.textView3);
+        SearchView searchView = (SearchView) findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                textView.setText(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
     }
 }
