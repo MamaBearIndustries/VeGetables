103cc34e94b51b66936e0df4e31a816555a1a3ee
vspenubarthi <vspenubarthi@wpi.edu>
2018-04-03 08:27:56 -0400

    This has changes to the main activity to make it easier to get to my Page, and it also has a semi-functional search bar

--- app/src/main/java/com/mamabearindustries/vegetables/MainActivity.java
+++ app/src/main/java/com/mamabearindustries/vegetables/MainActivity.java
@@ -6,13 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myRef.setValue("Hello, World!");
         Button button = (Button) findViewById(R.id.button_search);
     }
     public void onClick(View v)
