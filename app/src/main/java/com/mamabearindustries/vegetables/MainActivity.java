d77c6efd884e469bb3c3ee115225c19ad096075b
vspenubarthi <vspenubarthi@wpi.edu>
2018-04-04 07:57:13 -0400

    This has changes to the main activity to make it easier to get to my Page, and it also has a semi-functional search bar

--- app/src/main/java/com/mamabearindustries/vegetables/MainActivity.java
+++ app/src/main/java/com/mamabearindustries/vegetables/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("message");
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
