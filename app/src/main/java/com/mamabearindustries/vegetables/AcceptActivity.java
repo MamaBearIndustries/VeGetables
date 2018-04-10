4355dc6ba2555e3318c765d47d15f77d88a4d528
gopinathans <amgopinathan@wpi.edu>
2018-04-10 11:59:27 -0400

    The second part is coming together.

--- app/src/main/java/com/mamabearindustries/vegetables/AcceptActivity.java
+++ app/src/main/java/com/mamabearindustries/vegetables/AcceptActivity.java
@@ -2,6 +2,7 @@ package com.mamabearindustries.vegetables;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableRow;
 
 public class AcceptActivity extends AppCompatActivity {
 
@@ -9,6 +10,7 @@ public class AcceptActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accept);
+
 
     }
 }
