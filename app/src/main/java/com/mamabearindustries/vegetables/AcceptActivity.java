bce8204ad49bbec1da97e6a29e2bb905f34de767
gopinathans <amgopinathan@wpi.edu>
2018-04-11 13:18:20 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/mamabearindustries/vegetables/AcceptActivity.java
+++ app/src/main/java/com/mamabearindustries/vegetables/AcceptActivity.java
@@ -2,7 +2,8 @@ package com.mamabearindustries.vegetables;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TableRow;
+import android.view.View;
+import android.widget.TextView;
 
 public class AcceptActivity extends AppCompatActivity {
 
@@ -12,5 +13,16 @@ public class AcceptActivity extends AppCompatActivity {
         setContentView(R.layout.activity_accept);
 
 
+    }
+
+    public void previousRequest(View v)
+    {
+        TextView pastFoodPantry = findViewById(R.id.previousFoodPantry);
+        TextView pastDate = findViewById(R.id.previousDate);
+        TextView pastAddress = findViewById(R.id.previousAddress);
+        TextView pastEmail = findViewById(R.id.pastEmail);
+        TextView pastPhone = findViewById(R.id.previousPhoneNumber);
+        TextView pastQuantity = findViewById(R.id.previousQuantity);
+        TextView pastItem = findViewById(R.id.previousItem);
     }
 }
