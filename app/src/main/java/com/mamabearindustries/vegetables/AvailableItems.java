dcb21975f603c0c1c271c5bb0421533a43ca82f1
gopinathans <amgopinathan@wpi.edu>
2018-04-17 11:04:12 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/mamabearindustries/vegetables/AvailableItems.java
+++ app/src/main/java/com/mamabearindustries/vegetables/AvailableItems.java
@@ -4,5 +4,50 @@ package com.mamabearindustries.vegetables;
  * Created by gopinathans on 4/17/18.
  */
 
-public class AvailableItems {
+public class AvailableItems
+{
+    private Food food;
+    private GroceryStore groceryStore;
+
+    public AvailableItems(Food food, GroceryStore groceryStore)
+    {
+        this.food = food;
+        this.groceryStore = groceryStore;
+    }
+
+    /**
+     * Gets the food item
+     * @return the food item
+     */
+    public Food getFood()
+    {
+        return food;
+    }
+
+    /**
+     * Sets the food item
+     * @param food the food item
+     */
+    public void setFood(Food food)
+    {
+        this.food = food;
+    }
+
+    /**
+     * Gets the groceryStore item
+     * @return the groceryStore item
+     */
+    public GroceryStore getGroceryStore()
+    {
+        return groceryStore;
+    }
+
+    /**
+     * Sets the groceryStore item
+     * @param groceryStore the groceryStore item
+     */
+    public void setGroceryStore(GroceryStore groceryStore)
+    {
+        this.groceryStore = groceryStore;
+    }
 }
