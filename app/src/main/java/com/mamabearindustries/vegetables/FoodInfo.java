68ed17ef99a510d10c7026f1b4f55f5a0a74cf74
admin <tfanwpi@gmail.com>
2018-04-20 12:58:53 -0400

    Added Food Pantry and Grocery Store classes

--- app/src/main/java/com/mamabearindustries/vegetables/FoodInfo.java
+++ app/src/main/java/com/mamabearindustries/vegetables/FoodInfo.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.util.Map;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FoodInfo extends AppCompatActivity {
 
@@ -18,20 +22,42 @@ public class FoodInfo extends AppCompatActivity {
 
         EditText itemNameText = findViewById(R.id.enterItemName);
         EditText quantityText = findViewById(R.id.enterQuantity);
-        EditText dateText = findViewById(R.id.enterExpDate);
+
 
         String itemName = itemNameText.getText().toString();
         int quantity = Integer.parseInt(quantityText.getText().toString());
-        String date = dateText.getText().toString();
 
-        Food food = new Food(itemName, quantity, date);
+        Food food = new Food(itemName, quantity);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("https://vegetables-1107.firebaseio.com/Message");
+        DatabaseReference usersRef = ref.child("users");
 
 
         Intent intent = new Intent(this, CurrentInventory.class);
         intent.putExtra("itemName", itemName);
         intent.putExtra("quantity", quantity);
-        intent.putExtra("date", date);
         startActivity(intent);
+
+        /*
+
+Map<String, User> users = new HashMap<>();
+users.put("alanisawesome", new User("June 23, 1912", "Alan Turing"));
+users.put("gracehop", new User("December 9, 1906", "Grace Hopper"));
+
+usersRef.setValueAsync(users);
+        Firebase usersRef = ref.child("Users");
+        Map<String, String> userData = new HashMap<String, String>();
+
+        userData.put("Nombre", name);
+        userData.put("Password", pass);
+        userData.put("Confirmed", "FALSE");
+        userData.put("Email", mail);
+
+        usersRef.setValue(name);
+        usersRef = ref.child("Users").child(name);
+        usersRef.setValue(userData);
+        */
     }
 
 
