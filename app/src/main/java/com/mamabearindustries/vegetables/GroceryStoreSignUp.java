de2c7873dbf5ec13d488dd0d88e6dc869c603d9d
vspenubarthi <vspenubarthi@wpi.edu>
2018-04-27 15:03:47 -0400

    This has the updated GroceryStoreSignUp Screen COMPLETED

--- app/src/main/java/com/mamabearindustries/vegetables/GroceryStoreSignUp.java
+++ app/src/main/java/com/mamabearindustries/vegetables/GroceryStoreSignUp.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -18,13 +19,23 @@ public class GroceryStoreSignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grocery_store_sign_up);
-       myGroceryStore = new GroceryStore("test","test","test","test","test","test","test");
-      final DatabaseReference stores = database.getReference().child("Grocery Stores").child(myGroceryStore.getStoreName()).child("Info");
+       final EditText name = findViewById(R.id.enter_store_name);
+        final EditText address = findViewById(R.id.enter_store_address);
+        final EditText phoneNumber = findViewById(R.id.enter_store_phone_number);
+        final EditText contactName = findViewById(R.id.enter_store_contact_name);
+        final EditText contactEmail = findViewById(R.id.enter_store_contact_email);
+        final  EditText username = findViewById(R.id.enter_username);
+        final EditText password = findViewById(R.id.enter_password);
 
-        Button storeSignup = findViewById(R.id.SignUp);
+        Button storeSignup = findViewById(R.id.Sign_up);
         storeSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                myGroceryStore = new GroceryStore(name.getText().toString(),address.getText().toString(),phoneNumber.getText().toString(),contactName.getText().toString(),contactEmail.getText().toString(),username.getText().toString(),password.getText().toString());
+                final DatabaseReference stores = database.getReference().child("Grocery Stores").child(myGroceryStore.getStoreName()).child("Info");
+
+
                 Map<String, Object> storeInfo = new HashMap<>();
                 storeInfo.put("Address", myGroceryStore.getAddress());
                 storeInfo.put("Phone Number", myGroceryStore.getPhoneNumber());
@@ -36,8 +47,6 @@ public class GroceryStoreSignUp extends AppCompatActivity {
                 stores.updateChildren(storeInfo);
             }
         });
-
-
 
     }
 }
